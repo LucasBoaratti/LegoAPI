70fb8ed20490d41be0b80e923f648a798329f2f1
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-01 02:00:42 -0300

    Rotas adicionadas: página de criar personagens e de editar personagens

--- FrontEnd/src/Routes/Rotas.jsx
+++ FrontEnd/src/Routes/Rotas.jsx
@@ -1,13 +1,22 @@
-import { Cabecalho } from "../Components/Cabecalho";
+import { CriarPersonagem } from "../Pages/CriarPersonagem";
 import { Home } from "../Pages/Home";
 import { Index } from "../Pages/Index";
 import { Routes, Route } from "react-router-dom";
+import { EditarPersonagem } from "../Pages/EditarPersonagem";
 
 export function Rotas() {
     return (
         <Routes>
             <Route path="/" element={<Index/>}>
                 <Route index element={<Home/>}/>
+            </Route>
+
+            <Route path="/criarPersonagem" element={<Index/>}>
+                <Route index element={<CriarPersonagem/>}/>
+            </Route>
+
+            <Route path="/editarPersonagem" element={<Index/>}>
+                <Route index element={<EditarPersonagem/>}/>
             </Route>
         </Routes>
     );
