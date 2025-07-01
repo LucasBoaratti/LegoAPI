5cad630e6d48a747848f6342f5888d8edc651115
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-01 01:57:32 -0300

    Trocando o nome e a senha do banco de dados para demonstração

--- BackEnd/Core/Configs.py
+++ BackEnd/Core/Configs.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base
 
 class Settings(BaseSettings):
      API_V1_STR: str = "/api/v1"
-     DB_URL: str = "mysql+asyncmy://root:lucas@127.0.0.1:3306/lego"
+     DB_URL: str = "mysql+asyncmy://{usuario_bd}:{senha_usuario_bd}@127.0.0.1:3306/lego"
      
      DBBaseModel = declarative_base()
      
